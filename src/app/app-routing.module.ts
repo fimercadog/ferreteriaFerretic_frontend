6479272445736ff9b54a78c5a775dbe407e44dff
juanseb7 <jsgp.071293@gmail.com>
2022-11-11 18:44:22 -0500

    Ready login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
 import {NgModule} from "@angular/core";
 import {HomeComponent} from "./home/home.component";
+import {AuthGuard} from "./providers/auth.guard";
 
 const routes:Routes = [
-  {path:'home', component:HomeComponent},
+  {path:'home', component:HomeComponent, canActivate:[AuthGuard]},
   {path:'login', component:LoginComponent},
   {path:'**', redirectTo:'/login'},
 ];
